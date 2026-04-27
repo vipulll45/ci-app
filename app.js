586a155a618203777f66e8d874287154ce735464
VIPYL PURUSHOTTAM CHAVHAN <168461846+vipulll45@users.noreply.github.com>
2026-04-27 23:03:43 +0530

    Update response message in root route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD Pipeline 🚀');
+  res.send(''Feature branch update 🚀');
 });
 
 app.listen(3000, () => {
